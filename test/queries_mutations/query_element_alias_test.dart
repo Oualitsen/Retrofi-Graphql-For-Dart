7bb7d3896ab2c0c4e4d423ed55626d90ff7ac960
Ramadan Oualitsen <ramadan.oualitsen.external@ynap.mail.onmicrosoft.com>
2023-07-04 17:49:08 +0100

    adds query alias tests

---
+++ test/queries_mutations/query_element_alias_test.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:retrofit_graphql/src/gq_grammar.dart';
+import 'package:petitparser/petitparser.dart';
+
+void main() async {
+  test("fail_on_absence_of_query_projection_over_type_test", () {
+    final GQGrammar g = GQGrammar();
+
+    var parser = g.buildFrom(g.fullGrammar().end());
+
+    final text = File("test/queries_mutations/query_element_alias_test.graphql")
+        .readAsStringSync();
+    var r = parser.parse(text);
+    expect(r.isSuccess, true);
+    expect(g.projectedTypes.keys, contains("DriverResponse"));
+    var response = g.projectedTypes["DriverResponse"]!;
+
+    expect(
+        response.fields.where((field) => field.name == "driver"), isNotEmpty);
+  });
+}
